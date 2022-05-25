94b623490fc72568e9c6923e7c0ae18497072f02
Tatsuki <Tatsuki@Narumis-MacBook-Pro.local>
2022-05-24 23:01:59 -0600

    cleaned up some commented-out lines

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -29,54 +29,6 @@ db.init_app(app)
 migrate = Migrate(app, db, compare_type = True)
 
 #----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-# class Venue(db.Model):
-#   __tablename__ = 'venues'
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String)
-#   city = db.Column(db.String(120))
-#   state = db.Column(db.String(120))
-#   address = db.Column(db.String(120))
-#   phone = db.Column(db.String(120))
-#   image_link = db.Column(db.String(500))
-#   facebook_link = db.Column(db.String(120))
-#   genres = db.Column(db.PickleType)
-#   website = db.Column(db.String(120))
-#   seeking_talent = db.Column(db.Boolean)
-#   seeking_description = db.Column(db.String)
-#   shows  = db.relationship('Show', backref='venue', lazy=True)
-#   def __repr__(self):
-#     return f'<venue id: {self.id}, name: {self.name}>'
-
-# class Artist(db.Model):
-#   __tablename__ = 'artists'
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String)
-#   city = db.Column(db.String(120))
-#   state = db.Column(db.String(120))
-#   phone = db.Column(db.String(120))
-#   genres = db.Column(db.PickleType)
-#   image_link = db.Column(db.String(500))
-#   facebook_link = db.Column(db.String(120))
-#   website = db.Column(db.String(120))
-#   seeking_venue = db.Column(db.Boolean)
-#   seeking_description = db.Column(db.String)
-#   shows = db.relationship('Show', backref='artist', lazy=True)
-#   def __repr__(self):
-#     return f'<artist id: {self.id}, name: {self.name}>'
-
-# class Show(db.Model):
-#   __tablename__ = 'shows'
-#   id = db.Column(db.Integer, primary_key=True)
-#   artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable = False)
-#   venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable = False)
-#   start_time = db.Column(db.DateTime())
-#   def __repr__(self):
-#     return f'< show id: {self.id}, artist id: {self.artist_id}, venue id: {self.venue_id}, start time: {self.start_time}>'
-
-#----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
 
